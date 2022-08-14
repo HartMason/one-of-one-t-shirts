3261dcbf729f276c7480cd4538fa70f5c22d0842
HartMason <hartmason85@gmail.com>
2022-08-13 21:23:54 -0500

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import ResponsiveAppBar from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <ResponsiveAppBar/>
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        <Router/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Router = () => {
+  return (
+    <Routes>
+        
+    </Routes>
+
+  )
+}
+
+export default Router
