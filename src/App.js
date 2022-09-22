eb8198a37d8d4598c27e02b030406bcc4644621b
HartMason <hartmason85@gmail.com>
2022-09-22 15:35:32 -0500

    background attachment to body solved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import backGround from "./image/backGround.jpg"
 
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="backgroundImage" style={{ backgroundImage: `url(${backGround})`,
+    position: 'fixed',
+    minWidth: '100%',
+    // backgroundSize: 'contain',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100%'
+  }}>
       <BrowserRouter>
+      <Navbar />
         <Router />
 
-        <Footer />
+        <Footer/>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// background-size: 'contain',
+// background-size: 'cover',
+// background-position: 'center',
+// background-repeat: 'no-repeat',
+// min-height: '100%'
