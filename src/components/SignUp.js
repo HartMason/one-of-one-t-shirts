179b6ebf4288ad588cd3ae87e4620a310598e67c
HartMason <hartmason85@gmail.com>
2022-10-08 10:07:49 -0500

    took away nightsky

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./About.css";
-import NightSky from "../image/NightSky.jpg"
+// import NightSky from "../image/NightSky.jpg"
 
 
 const SignUp = () => {
   return (  
   <div style={{
-    backgroundImage: `url(${NightSky})`,
+    // backgroundImage: `url(${NightSky})`,
     maxWidth: "100%",
     height: "800px",
     backgroundSize: "cover",
